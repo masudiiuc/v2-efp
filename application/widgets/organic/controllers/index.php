f0598611e318711aa238d26b45b301c7223ace8e
M.M.H.Masud <masudiiuc@gmail.com>
2012-09-05 23:24:05 +0600

    First commit with Framework setup

---
+++ application/widgets/organic/controllers/index.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Index extends MX_Controller
+{
+    protected $data;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->layout->setLayout('layout/organic');
+    }
+
+    public function home()
+    {
+        $this->layout->view('home');
+    }
+}
---
+++ application/widgets/organic/views/layout/organic.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Eco-Friendly Party&trade;</title>
+
+    </head>
+
+<body>
+    <div id="header>">
+
+    </div>
+
+
+    <div id="content">
+
+        <div id="show-message">
+            <?php $this->message->display();  ?>
+        </div>
+
+        <?php echo $content_for_layout ?>
+
+    </div>
+
+    <div id="footer">
+
+    </div>
+
+</body>
+</html>
