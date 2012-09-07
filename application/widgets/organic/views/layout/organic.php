bac4180bcd951e568f4ee85ebb9df58b5e0e77ed
M.M.H.Masud <masudiiuc@gmail.com>
2012-09-07 13:13:50 +0600

    remove unwantr git files. added new layout of organic listing

--- application/widgets/organic/views/layout/organic.php
+++ application/widgets/organic/views/layout/organic.php
@@ -3,6 +3,7 @@
         <title>Eco-Friendly Party&trade;</title>
 
         <link href="<?php echo base_url() ?>public/bootstrap/css/bootstrap.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url() ?>public/css/style.css" media="screen" rel="stylesheet" type="text/css" />
     </head>
 
 <body>
@@ -35,71 +36,68 @@
             <div class="span9">
 
                 <div class="row-fluid">
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
                     </div><!--/span-->
-
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
                     </div><!--/span-->
-
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
                     </div><!--/span-->
-                </div><!--/row-->
-
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                </div>
                 <div class="row-fluid">
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
                     </div><!--/span-->
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
                     </div><!--/span-->
-                    <div class="span4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        <p><a class="btn" href="#">View details &raquo;</a></p>
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
                     </div><!--/span-->
-                </div><!--/row-->
-            </div><!--/span-->
+                    <div class="span3 widget-box">
+                        <h4>Captains Eco Provider</h4>
+                        <h6><b>organic beer</b></h6>
+                        <p>234x park lane, brea, CA-11105</p>
+                        <p><a class="btn-mini fr" href="#">View details &raquo;</a></p>
+                    </div><!--/span-->
+                </div>
 
-            <div class="span3">
-                <div class="well sidebar-nav">
-                    <ul class="nav nav-list">
-                        <li class="nav-header">Sidebar</li>
-                        <li class="active"><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li class="nav-header">Sidebar</li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li class="nav-header">Sidebar</li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                        <li><a href="#">Link</a></li>
-                    </ul>
-                </div><!--/.well -->
-            </div><!--/span-->
-        </div><!--/row-->
+            </div>
 
-        <hr>
+            <div class="span3 left-sidebar-top">
 
-        <footer>
+                <?php $this->load->view('sections/left-sidebar.php'); ?>
+
+            </div>
+        </div>
+
+        <footer class="footer">
             <p>&copy; Company 2012</p>
         </footer>
 
---
+++ application/widgets/organic/views/layout/sections/left-sidebar.php
@@ -0,0 +1,50 @@
+<div class="well sidebar-nav left-sidebar-bg">
+    <div class="sidebar-box">
+        <form>
+            <legend>Search for Eco-Vendors</legend>
+            <label>Find</label>
+            <select id="listing-type" name="listing_type">
+                <option>Select one</option>
+            </select>
+
+            <label>Location</label>
+            <input type="text" class="input-medium" placeholder="city, state or Zip code">
+
+
+            <label>Within</label>
+            <select id="radius" name="radius">
+                <option value="5">5 miles</option>
+            </select>
+
+            <button type="submit" class="btn-small">Search Now!</button>
+        </form>
+    </div>
+
+    <div class="sidebar-box">
+        <form>
+            <legend>Login Eco-Vendor</legend>
+
+            <label>Email address </label>
+            <div class="input-prepend">
+                <span class="add-on">@</span><input class="span9" id="email" size="16" type="text" placeholder="Enter Email">
+            </div>
+
+
+            <label>Password </label>
+            <div class="input-prepend">
+                <span class="add-on">**</span><input class="span9" id="password" size="16" type="password" placeholder="Enter password">
+            </div>
+
+            <button type="submit" class="btn-small">Login</button>
+        </form>
+    </div>
+
+    <div class="sidebar-box">
+        <form>
+            <legend>Eco-Friendly Approval Seal</legend>
+
+            <img src="<?php echo base_url() ?>public/images/eco-friendly-party-seal.png" width="60%" align="middle" style="padding-left: 45px"/>
+
+        </form>
+    </div>
+</div><!--/.well -->
